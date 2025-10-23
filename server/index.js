82612c71694e20fd99d625216c72eedeb1610f21
Donatien Vachette <donatien.vachette@hotmail.com>
2025-10-23 16:12:32 +0200

    Route ajout d'utilisateur [POST]`/user`

--- server/index.js
+++ server/index.js
@@ -1,15 +1,19 @@
 const express = require('express'); // Importation du framework Express
-
+const userRoutes = require('./routes/user.js'); // Importation des routes utilisateur
 const cors = require('cors'); // Importation de l'outil CORS pour gérer les requêtes multi-origines
 
 const PORT = process.env.PORT || 3000; // Définition du port d'écoute du serveur
 
 const app = express(); // Création de l'application Express
 
+
 app.use(cors()); // Activation de CORS pour toutes les routes
+app.use(express.json()); // Outil pour parser le JSON dans les requêtes entrantes
+app.use(express.urlencoded({ extended: true })); // Outil pour parser les données URL-encoded
+
 
 // Routes pour les utilisateurs
-
+app.post('/user', userRoutes.createAccount);
 
 
 
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,17 +1,37 @@
-const database = require('../include/database.js');
+const Database = require('better-sqlite3');
+
+
 function createAccount(request, response) {
     if (request.method !== 'POST') {
         return response.status(405).send({ error: 'Methode non autorisée' });
     }
-    const mail = request.body.mail;
+
+    const mail = request.body.email;
     const password = request.body.password;
-    const username = request.body.username;
+    const username = request.body.name;
 
     if (!mail || !password || !username) {
         return response.status(400).send({ error: 'Information manquante' });
     }
-    const db = database.openDatabase('database.db');
-    const checkUserStmt = db.prepare('SELECT COUNT(*) AS count FROM users WHERE mail = ?');
+    const db = new Database('database.db')
+
+    const checkMailPseudo = db.prepare('SELECT * FROM user WHERE email = ? OR name = ?');
+    const existingUser = checkMailPseudo.get(mail, username);
+    
+    if (existingUser) {
+        return response.status(409).send({ error: 'Mail ou pseudo déjà utilisé' });
+    }
+
+    const insertUser = db.prepare('INSERT INTO user (email, password, name) VALUES (?, ?, ?)');
+    insertUser.run(mail, password, username);
+
+    const newUser = db.prepare('SELECT userid, email, name FROM user WHERE email = ?').get(mail);
+
+    return response.status(201).send({ message: 'Compte créé avec succès', user: newUser });
+    
     
 }
 
+module.exports = { 
+    createAccount
+};
