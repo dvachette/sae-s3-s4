26ced23a58861e44e140abc17a86a62112f96c5b
Aloys <aloysgayet@gmail.com>
2025-10-29 09:11:24 +0100

    Première version du css

---
+++ TCG_web/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <header>
+      <h1>Bienvenue sur BDE INFO TCG</h1>
+    </header>
+    <div class="page connexion">
+      <h2>Connexion :</h2>
+      <form @submit="connexion">
+        <div>
+          <label>
+            <input type="text" v-model="id_utilisateur" />Nom d'utilisateur ou
+            email
+          </label>
+        </div>
+        <div>
+          <label>
+            <input type="password" v-model="mdp_utilisateur" />Mot de passe
+          </label>
+        </div>
+        <button type="submit">Se Connecter</button>
+      </form>
+      <button>Créer un compte</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+</script>
