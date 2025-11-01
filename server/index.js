0a225aa701de3688437ee597c9c0610af1c06257
Donatien Vachette <donatien.vachette@hotmail.com>
2025-11-01 01:03:41 +0100

    Commentaires sur

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
+/** 
+    @file : server/index.js
+    @author : Elise FOUR, Donatien VACHETTE
+    @brief : Point d'entrée du serveur Express. 
+    @description : Configure les outils intermédiaires, définit les routes et démarre le serveur.
+*/
+
+// Modules NPM
 const express = require('express'); // Importation du framework Express
 const session = require('express-session'); // Importation de l'outil de gestion de sessions
 const cors = require('cors'); // Importation de l'outil CORS pour gérer les requêtes multi-origines
-
+// Modules internes
 const friendsRoutes = require('./routes/friends.js'); // Importation des routes d'amis
 const userRoutes = require('./routes/user.js'); // Importation des routes utilisateur
-
-
 
 
 const PORT = process.env.PORT || 3000; // Définition du port d'écoute du serveur
@@ -30,13 +36,15 @@ app.use(cors()); // Activation de CORS pour toutes les routes
 app.use(express.json()); // Outil pour parser le JSON dans les requêtes entrantes
 app.use(express.urlencoded({ extended: true })); // Outil pour parser les données URL-encoded
 
+
+// Routes de l'application
+
+
 // Routes pour la gestion des amis
 app.post('/friends/request', friendsRoutes.requestFriend);
 app.get('/friends/requests', friendsRoutes.getPendingRequests);
-
 app.post('/friends/accept', friendsRoutes.acceptFriendRequest);
 app.post('/friends/reject', friendsRoutes.rejectFriendRequest);
-
 app.get('/friends', friendsRoutes.getFriendsList);
 app.delete('/friends', friendsRoutes.removeFriend);
 
@@ -44,12 +52,12 @@ app.delete('/friends', friendsRoutes.removeFriend);
 // Routes pour les utilisateurs
 app.post('/user', userRoutes.createAccount);
 app.put('/user', userRoutes.editAccount);
-
 app.post('/login', userRoutes.login);
 app.get('/logout', userRoutes.logout);
 
 
-// Démarrer le serveur
+// Démarrer le serveur sur le port spécifié (Environnement de developpement, pas en production)
 app.listen(PORT, () => {
+    console.log('ATTENTION, SERVEUR EN MODE DÉVELOPPEMENT, NE PAS UTILISER EN PRODUCTION !');
     console.log(`Server is running on port ${PORT}`);
 });
