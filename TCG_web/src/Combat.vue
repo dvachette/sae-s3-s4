e12e2c9594c144815d239cce82e6edcfc94c9a74
Aloys <aloysgayet@gmail.com>
2025-11-11 22:08:31 +0100

    Pages booster et combat

--- TCG_web/src/Combat.vue
+++ TCG_web/src/Combat.vue
@@ -1,12 +1,27 @@
 <template>
   <MonHeader />
-  <main>
-    <h2>LA BAGARRE !!!!!</h2>
+  <main id="Combat">
+    <div class="page_combat">
+      <div class="F_f">
+        <img src="@/assets/imgs/logoTCG.png" alt="Page F/f" />
+        <button>Forces et faiblesses</button>
+      </div>
+      <div class="lancement_partie">
+        <img src="@/assets/imgs/logoTCG.png" alt="Page F/f" />
+        <button>Jouer</button>
+      </div>
+      <div class="Compteur">
+        <clef />
+      </div>
+    </div>
+    <div class="deck"></div>
   </main>
 </template>
 
 <script setup>
 import { ref } from 'vue';
+import clef from '@/Composants/clef.vue';
+import '@/assets/css/combat.css';
 
 import MonHeader from '@/Composants/header.vue';
 </script>
---
+++ TCG_web/src/Composants/boutons_achat.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="bouton_achat">
+    <p>100</p>
+    <img src="@/assets/imgs/Clef.png" alt="Lot de clefs" />
+  </div>
+</template>
+
+<script setup>
+import '@/assets/css/bouton_achat.css';
+</script>
