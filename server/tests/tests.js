7ebc7ee50c135c176c1a3ef33a7ea66fdfe8c4e7
Donatien Vachette <donatien.vachette@hotmail.com>
2025-11-17 17:45:06 +0100

    tests

--- server/tests/tests.js
+++ server/tests/tests.js
@@ -1,5 +1,4 @@
 const Member = require('../objects/member.js');
-
-const member = Member.fromId(5, 1);
-console.log(member);
-console.log(member.attacks[0].effects);
+const Card = require('../objects/card.js');
+const cards = Card.getAll();
+console.log(cards);
