f9d72005fec1bc9b2eaa664868ab7b5df0b185f0
Nathalie <anq.nathalie@gmail.com>
2025-11-20 09:49:25 +0100

    affichage statique recompenses quotidiennes

--- TCG_web/src/Booster.vue
+++ TCG_web/src/Booster.vue
@@ -2,7 +2,38 @@
   <MonHeader />
   <main id="booster">
     <div class="Récompenses_quotidiennes">
-      <h2>Récompenses quotidiennes :</h2>
+      <h2>Récompenses quotidiennes</h2>
+      <div class="jour actif fait"  id="j1">
+        <p>Jour 1</p>
+        <p>20</p>
+        <img src="@/assets/imgs/Clef.png" alt="clé"/>
+      </div>
+      <div class="jour fait" id="j2">
+        <p>Jour 2</p>
+        <img src="@/assets/imgs/carte/carte_inconnue.png" alt="">
+      </div>
+      <div class="jour" id="j3">
+        <p>Jour 3</p>
+        <div class="Booster"></div>
+      </div>
+      <div class="jour" id="j4">
+        <p>Jour 4</p>
+        <img src="@/assets/imgs/carte/carte_inconnue.png" alt="carteInconnue">
+      </div>
+      <div class="jour" id="j5">
+        <p>Jour 5</p>
+        <p>70</p>
+        <img src="@/assets/imgs/cles.png" alt="clés"/>
+      </div>
+      <div class="jour" id="j6">
+        <p>Jour 6</p>
+        <img src="@/assets/imgs/carte/carte_inconnue.png" alt="carteInconnue">
+      </div>
+      <div class="jour" id="j7">
+        <p>Jour 7</p>
+        <p>3</p>
+        <div class="Booster"></div>
+      </div>
     </div>
     <div class="Booster_page">
       <div class="Booster"></div>
