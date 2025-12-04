0dd72071cf35ce6a9bfb96ae21c7274e5d91288d
Donatien Vachette <donatien.vachette@hotmail.com>
2025-12-04 23:27:39 +0100

    on va juste checkout

--- TCG_web/src/Composants/clef.vue
+++ TCG_web/src/Composants/clef.vue
@@ -1,22 +1,10 @@
 <template>
   <div class="clef">
-<<<<<<< HEAD
-    <p>{{ props.NbCle || 0 }}</p>
-=======
     <p>{{ props.cles }}</p>
->>>>>>> 42282d7a31c4a74c105585ce4bdff2d1d4bf48b4
     <img src="@/assets/imgs/Clef.png" alt="Lot de clefs" />
   </div>
 </template>
 
-<<<<<<< HEAD
-<script setup>
-const props = defineProps({
-  NbCle: Number,
-});
-</script>
-
-=======
 
 <script setup>
   const props = defineProps({
@@ -27,7 +15,6 @@ const props = defineProps({
     }
   });
 </script>
->>>>>>> 42282d7a31c4a74c105585ce4bdff2d1d4bf48b4
 <style scoped>
 @import '../assets/css/style_clef.css';
 </style>
