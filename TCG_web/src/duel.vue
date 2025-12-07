f7059be3b226aee9d37e807fcb1bfd7c8f57abc7
Aloys <aloysgayet@gmail.com>
2025-12-07 09:25:12 +0100

    Commit fantôme

--- TCG_web/src/duel.vue
+++ TCG_web/src/duel.vue
@@ -9,39 +9,39 @@
         </div>
         <button>Abandonner</button>
       </div>
-      <div class="cartes_combat">
-        <div class="mon_deck">
-          <div class="familier">
-            <p>Familier</p>
-            <Carte_membre largeur="100px" />
-          </div>
+    </div>
+    <div class="cartes_combat">
+      <div class="son_deck">
+        <div class="familier">
+          <p>Familier</p>
           <Carte_membre largeur="100px" />
-          <Vie_cartes :pv="5" :pvtotal="100" largeur="100px" />
-          <Carte_membre largeur="100px" />
-          <Vie_cartes :pv="74" :pvtotal="81" largeur="100px" />
-          <Carte_membre largeur="150px" />
-          <Vie_cartes :pv="30" :pvtotal="100" largeur="150px" />
-          <Carte_membre largeur="100px" />
-          <Vie_cartes :pv="74" :pvtotal="200" largeur="100px" />
-          <Carte_membre largeur="100px" />
-          <Vie_cartes :pv="74" :pvtotal="100" largeur="100px" />
         </div>
-        <div class="son_deck">
-          <div class="familier">
-            <p>Familier</p>
-            <Carte_membre largeur="100px" />
-          </div>
+        <Carte_membre largeur="100px" />
+        <Vie_cartes :pv="100" :pvtotal="100" largeur="100px" />
+        <Carte_membre largeur="100px" />
+        <Vie_cartes :pv="74" :pvtotal="120" largeur="100px" />
+        <Carte_membre largeur="150px" />
+        <Vie_cartes :pv="40" :pvtotal="100" largeur="150px" />
+        <Carte_membre largeur="100px" />
+        <Vie_cartes :pv="74" :pvtotal="100" largeur="100px" />
+        <Carte_membre largeur="100px" />
+        <Vie_cartes :pv="12" :pvtotal="100" largeur="100px" />
+      </div>
+      <div class="mon_deck">
+        <div class="familier">
+          <p>Familier</p>
           <Carte_membre largeur="100px" />
-          <Vie_cartes :pv="100" :pvtotal="100" largeur="100px" />
-          <Carte_membre largeur="100px" />
-          <Vie_cartes :pv="74" :pvtotal="120" largeur="100px" />
-          <Carte_membre largeur="150px" />
-          <Vie_cartes :pv="40" :pvtotal="100" largeur="150px" />
-          <Carte_membre largeur="100px" />
-          <Vie_cartes :pv="74" :pvtotal="100" largeur="100px" />
-          <Carte_membre largeur="100px" />
-          <Vie_cartes :pv="12" :pvtotal="100" largeur="100px" />
         </div>
+        <Carte_membre largeur="100px" />
+        <Vie_cartes :pv="5" :pvtotal="100" largeur="100px" />
+        <Carte_membre largeur="100px" />
+        <Vie_cartes :pv="74" :pvtotal="81" largeur="100px" />
+        <Carte_membre largeur="150px" />
+        <Vie_cartes :pv="30" :pvtotal="100" largeur="150px" />
+        <Carte_membre largeur="100px" />
+        <Vie_cartes :pv="74" :pvtotal="200" largeur="100px" />
+        <Carte_membre largeur="100px" />
+        <Vie_cartes :pv="74" :pvtotal="100" largeur="100px" />
       </div>
     </div>
   </main>
