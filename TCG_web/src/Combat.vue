8b6df49dfff668a1e8f17c1146433a1cbac056cc
Nathalie <anq.nathalie@gmail.com>
2025-12-28 01:56:21 +0100

    fin page combat deck statique (pas d'action de config deck)

--- TCG_web/src/Combat.vue
+++ TCG_web/src/Combat.vue
@@ -16,7 +16,7 @@
         <clef :cles="nbCles" />
       </div>
     </div>
-    <div class="deck">
+    <div class="deck" :style="{ '--hPage': hmain }">
       <p>Mon deck</p>
       <p>Familier</p>
       <p>Terrain</p>
@@ -30,23 +30,63 @@
         <Carte_membre
           v-else
           :data="carte"
-          largeur="150px"
+          largeur="200px"
         />
       </div>
-      <div id="separateur"></div>
+      <div class="separateur"></div>
 
-      <Carte_familier v-if="familier" :data="familier" largeur="150px"/>
+      <Carte_familier v-if="deckFamilier" :data="deckFamilier" largeur="200px"/>
       <img v-else src="@/assets/imgs/carte/carte_ajout.png" alt="Carte_à_Ajouter" id="familier">
-      <Carte_terrain v-if="terrain" :data="terrain" largeur="150px" />
+      <Carte_terrain v-if="deckTerrain" :data="deckTerrain" largeur="200px" />
       <img v-else src="@/assets/imgs/carte/carte_ajout.png" alt="Carte_à_Ajouter" id="terrain">
     </div>
 
+    <div class="separateurDeckCollection" :style="{ '--vhPx': vh+'px' }"></div>
+
+    <div class="collection">
+      <div class="collecMembre" :style="{ '--wgap': gapCollec }">
+        <Carte_membre v-for="carte of membres"
+          :key="carte.card.cardId"
+      
+        :data="carte.card" largeur="200px" class="membre"/>
+        <Carte_membre v-for="carte of membres"
+          :key="carte.card.cardId + 1"
+      
+        :data="carte.card" largeur="200px" class="membre"/>
+        <Carte_membre v-for="carte of membres"
+          :key="carte.card.cardId + 2"
+      
+        :data="carte.card" largeur="200px" class="membre"/>
+        <Carte_membre v-for="carte of membres"
+          :key="carte.card.cardId + 3"
+      
+        :data="carte.card" largeur="200px" class="membre"/>
+      </div>
+
+      <div class="separateur"></div>
+
+      <div class="collecFamilier">
+        <Carte_familier v-for="carte of familiers"
+          :key="carte.card.cardId"
+      
+        :data="carte.card" largeur="200px" class="familier"/>
+      </div>
+      
+
+      <div class="collecTerrain">
+        <Carte_terrain v-for="carte of terrains"
+          :key="carte.card.cardId"
+          
+            :data="carte.card" largeur="200px" class="terrain"/>
+      </div>
+      
+    </div>
     
   </main>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import clef from '@/Composants/clef.vue';
 
 import MonHeader from '@/Composants/header.vue';
@@ -55,16 +95,35 @@ import Carte_familier from './Composants/carte_familier.vue';
 import Carte_terrain from './Composants/carte_terrain.vue';
 
 const vh = ref(window.innerHeight / 100); //obtenir 1% de la hauteur de la fenetre, en px
-const hmain = ref(vh.value * 87.5 - 208.5 - 20*2 + 'px');
+const vw = ref(window.innerWidth / 100);
+const hmain = ref(vh.value * 87.5 - 278 - 20*2 + 'px');
+const gapCollec = ref(((vw.value * 100 - 40) * 0.7 - (200 * 5))/5 + 'px') //(100vw - padding de div deck)*0.7 pour en avoir 70% (14*5) - l'espace pris par les 5 cartes / tout divisé par 5 pour avoir un seul espace
 
 const userData = ref(JSON.parse(localStorage.getItem('userData'))); //OK
 const nbCles = ref(userData.value.balance); 
 
 const deckMembre = userData.value.deck.cards;
-const familier = userData.value.deck.pet;
-const terrain = userData.value.deck.arena;
+const deckFamilier = userData.value.deck.pet;
+const deckTerrain = userData.value.deck.arena;
 
-const collection = userData.value.collection;
+const collection = computed(() => { return userData.value.collection;});
+const membres = computed(() => {
+  return collection.value.filter( 
+    carte => carte.card._class === 'member' 
+  )
+})
+const familiers = computed(() => { //computed : prend une fonction en parametre : elle est recalculée dès que collection change
+  return collection.value.filter( 
+    carte => carte.card._class === 'pet' //condition fonction fléchée)qui doit etre a true pour etre selectionnée par filter
+    // carte est l'élément examiné par filter lorsqu'il parcours collection
+  )
+})
+const terrains = computed(() => {
+  return collection.value.filter( 
+    carte => carte.card._class === 'arena' 
+  )
+})
+console.log(familiers.value[0].card)
 
 </script>
 
