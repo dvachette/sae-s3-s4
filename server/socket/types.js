60b34d7bfefb7c0f8649cef83dc61ef7efa74a55
Donatien Vachette <donatien.vachette@hotmail.com>
2026-01-02 21:56:03 +0100

    effets de statut

--- server/socket/types.js
+++ server/socket/types.js
@@ -97,30 +97,184 @@ export class CombatState {
         this.tour += 1;
         const toPlayPlayer = this.tour % 2 === 1 ? this.player1 : this.player2;
         toPlayPlayer.energie += 1;
-    }
-
-    attack(attackerId, attackIndex) {
-        const attacker = this.player1.userId === attackerId ? this.player1 : this.player2;
-        const defender = this.player1.userId === attackerId ? this.player2 : this.player1;
-        const attack = attacker.main.carteActive.attacks[attackIndex];
-      
-        if (attack.cost <= attacker.energie) {
-            for (const effect of attack.effects) {
-                if (effect.type === 'damage') {
-                    let cardsToDamage = this.selectCards(attackerId, effect.target);
-                    for (const card of cardsToDamage) {
+        for (const card of [
+            toPlayPlayer.main.carte1,
+            toPlayPlayer.main.carte2,
+            toPlayPlayer.main.carteActive,
+            toPlayPlayer.main.carte4,
+            toPlayPlayer.main.carte5
+        ]) {
+            // Appliquer les effets de statut poison et régénération
+            for (const effect of card.statusEffects) {
+                switch (effect.type) {
+                    case 'poison':
                         card.hitPoints -= effect.value;
                         if (card.hitPoints < 0) {
                             card.hitPoints = 0;
                         }
-                    }
+                        break;
+                    case 'regen':
+                        card.hitPoints += effect.value;
+                        if (card.hitPoints > card.maxHitpoints) {
+                            card.hitPoints = card.maxHitpoints;
+                        }
+                        break;
+                    default:
+                        break;
                 }
             }
-            attacker.energie -= attack.cost;
+        }
+
+        // Mettre à jour la durée des effets de statut du joueur dont le tour vient de se terminer
+        const finishedPlayer = this.tour % 2 === 1 ? this.player2 : this.player1;
+        for (const card of [
+            finishedPlayer.main.carte1,
+            finishedPlayer.main.carte2,
+            finishedPlayer.main.carteActive,
+            finishedPlayer.main.carte4,
+            finishedPlayer.main.carte5
+        ]) {
+            card.statusEffects = card.statusEffects.filter(effect => {
+                if (effect.duration) {
+                    effect.duration -= 1;
+                    return effect.duration > 0;
+                }
+                return true; // Effets sans durée restent
+            });
+        }
+            
+    }
+
+    attack(attackerId, attackIndex) {
+
+        const attacker = this.player1.userId === attackerId ? this.player1 : this.player2;
+        const defender = this.player1.userId === attackerId ? this.player2 : this.player1;
+        const attack = attacker.main.carteActive.attacks[attackIndex];
+        if (!attack) {
+            return false; // Attaque invalide
+        }
+
+        if (attacker.main.carteActive.statusEffects.some(se => se.type === 'stun')) {
+            return false; // La carte est étourdie et ne peut pas attaquer
+        }
+        const energyCostBonus = attacker.main.carteActive.statusEffects
+            .filter(se => se.type === 'rage')
+            .reduce((acc, se) => acc - se.value, 0);
+        const energyCostPenalty = attacker.main.carteActive.statusEffects
+            .filter(se => se.type === 'fatigue')
+            .reduce((acc, se) => acc + se.value, 0);
+        const cost = Math.max(0, attack.cost + energyCostPenalty - energyCostBonus);
+        if (cost <= attacker.energie) {
+            for (const effect of attack.effects) {
+                const cardsToAffect = this.selectCards(attackerId, effect.target);
+                for (const card of cardsToAffect) {
+                    switch (effect.type) {
+                        case 'damage':
+                            if (card.statusEffects.some(se => se.type === 'shield')) { // Vérifier si la carte a un effet de bouclier
+                                card.statusEffects.find(se => se.type === 'shield').value -= effect.value; // Réduire la valeur du bouclier
+                                if (card.statusEffects.find(se => se.type === 'shield').value <= 0) { // Si le bouclier est épuisé, le retirer
+                                    card.statusEffects = card.statusEffects.filter(se => se.type !== 'shield');
+                                }
+                                break; // Sortir du switch sans infliger de dégâts
+                            }
+                            const damageBonus = attacker.main.carteActive.statusEffects
+                                .filter(se => se.type === 'strength')
+                                .reduce((acc, se) => acc + se.value, 0);
+                            const damagePenalty = attacker.main.carteActive.statusEffects
+                                .filter(se => se.type === 'weakness')
+                                .reduce((acc, se) => acc + se.value, 0);
+                            const totalDamage = Math.max(0, effect.value + damageBonus - damagePenalty);
+                            card.hitPoints -= totalDamage
+                            if (card.hitPoints < 0) {
+                                card.hitPoints = 0;
+                            }
+                            break;
+                        case 'heal': 
+                            card.hitPoints += effect.value;
+                            if (card.hitPoints > card.maxHitpoints) {
+                                card.hitPoints = card.maxHitpoints;
+                            }
+                            break;
+                        case 'shield':
+                            if (card.statusEffects.some(se => se.type === 'shield')) {
+                                card.statusEffects.find(se => se.type === 'shield').value = Math.max(card.statusEffects.find(se => se.type === 'shield').value, effect.value); // Assurer que les boucliers ne s'empilent pas
+                            } else {
+                                card.statusEffects.push({ type: 'shield', value: effect.value });
+                            }
+                            break;
+                        
+                        case 'poison':
+                            if (card.statusEffects.some(se => se.type === 'poison')) {
+                                card.statusEffects.find(se => se.type === 'poison').value = Math.max(card.statusEffects.find(se => se.type === 'poison').value, effect.value); // Assurer que les poisons ne s'empilent pas
+                                card.statusEffects.find(se => se.type === 'poison').duration = Math.max(card.statusEffects.find(se => se.type === 'poison').duration, effect.duration);
+                            } else {
+                                card.statusEffects.push({ type: 'poison', value: effect.value, duration: effect.duration });// met du dégat sur plusieurs tours
+                            }
+                            break;
+                        case 'stun':
+                            if (card.statusEffects.some(se => se.type === 'stun')) {
+                                card.statusEffects.find(se => se.type === 'stun').duration = Math.max(card.statusEffects.find(se => se.type === 'stun').duration, effect.duration);
+                            } else {
+                                card.statusEffects.push({ type: 'stun', duration: effect.duration }); // empèche d'attaquer
+                            }    
+                            break;
+                        case 'rage':
+                            if (card.statusEffects.some(se => se.type === 'rage')) {
+                                card.statusEffects.find(se => se.type === 'rage').value = Math.max(card.statusEffects.find(se => se.type === 'rage').value, effect.value); // attaques coutent moins cher
+                                card.statusEffects.find(se => se.type === 'rage').duration = Math.max(card.statusEffects.find(se => se.type === 'rage').duration, effect.duration);
+                            } else {
+                                card.statusEffects.push({ type: 'rage', value: effect.value, duration: effect.duration }); // attaques coutent moins cher
+                            }    
+                            break;
+                        case 'fatigue':
+                            if (card.statusEffects.some(se => se.type === 'fatigue')) {
+                                card.statusEffects.find(se => se.type === 'fatigue').value = Math.max(card.statusEffects.find(se => se.type === 'fatigue').value, effect.value); // attaques coutent plus cher
+                                card.statusEffects.find(se => se.type === 'fatigue').duration = Math.max(card.statusEffects.find(se => se.type === 'fatigue').duration, effect.duration);
+                            } else {
+                                card.statusEffects.push({ type: 'fatigue', value: effect.value, duration: effect.duration }); // attaques coutent plus cher
+                            }
+                            break;
+                        case 'strength':
+                            if (card.statusEffects.some(se => se.type === 'strength')) {
+                                card.statusEffects.find(se => se.type === 'strength').value = Math.max(card.statusEffects.find(se => se.type === 'strength').value, effect.value); // inflige plus de dégats
+                                card.statusEffects.find(se => se.type === 'strength').duration = Math.max(card.statusEffects.find(se => se.type === 'strength').duration, effect.duration);
+                            } else {
+                                card.statusEffects.push({ type: 'strength', value: effect.value, duration: effect.duration }); // inflige plus de dégats
+                            }    
+                            break;
+                        case 'weakness':
+                            if (card.statusEffects.some(se => se.type === 'weakness')) {
+                                card.statusEffects.find(se => se.type === 'weakness').value = Math.max(card.statusEffects.find(se => se.type === 'weakness').value, effect.value); // inflige moins de dégats
+                                card.statusEffects.find(se => se.type === 'weakness').duration = Math.max(card.statusEffects.find(se => se.type === 'weakness').duration, effect.duration);
+                            } else {
+                                card.statusEffects.push({ type: 'weakness', value: effect.value, duration: effect.duration }); // inflige moins de dégats
+                            }
+                            break;
+                        case 'regen':
+                            if (card.statusEffects.some(se => se.type === 'regen')) {
+                                card.statusEffects.find(se => se.type === 'regen').value = Math.max(card.statusEffects.find(se => se.type === 'regen').value, effect.value); // soigne chaque tour
+                                card.statusEffects.find(se => se.type === 'regen').duration = Math.max(card.statusEffects.find(se => se.type === 'regen').duration, effect.duration);
+                            } else {
+                                card.statusEffects.push({ type: 'regen', value: effect.value, duration: effect.duration }); // soigne chaque tour
+                            }
+                            break;
+                        default:
+                            console.log(`Unknown effect type: ${effect.type}`);
+                            break;
+                        
+                    } 
+
+                }
+            }
+            attacker.energie -= cost;
             return true;
         }
         return false;
     }
+
+
+
+
 
     /**
      * Sélectionne les cartes cibles en fonction du sélecteur fourni.
@@ -182,6 +336,8 @@ export class CombatState {
             opponent.main.carte4,
             opponent.main.carte5
         ];
+
+        console.log(`Selector : `, selector);
 
         for (const critere in selector) {
             switch (critere) {
@@ -363,7 +519,8 @@ export class CarteMembre {
     force; // Tableau d'id de type sur lesquels la carte est forte
     faiblesse; // Tableau d'id de type sur lesquels la carte est faible
     type; // Tableau d'id de types
-    constructor(name, hitPoints, maxHitpoints, attacks, description, mandat, force, faiblesse, type) {
+    statusEffects; // tableau d'effets actifs sur la carte
+    constructor(name, hitPoints, maxHitpoints, attacks, description, mandat, force, faiblesse, type, statusEffects) {
         this.name = name;
         this.hitPoints = hitPoints;
         this.maxHitpoints = maxHitpoints;
@@ -373,6 +530,7 @@ export class CarteMembre {
         this.force = force;
         this.faiblesse = faiblesse;
         this.type = type;
+        this.statusEffects = statusEffects;
     }
 }
 
