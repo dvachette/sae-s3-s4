d1be3fd0273b39e7ca24bf73aacb0c2d86c2c9ea
Aloys <aloysgayet@gmail.com>
2026-01-17 22:27:21 +0100

    gestion de mes echanges bientot finit

--- TCG_web/src/Composants/mes_echange.vue
+++ TCG_web/src/Composants/mes_echange.vue
@@ -1,42 +1,178 @@
 <template>
   <div class="mon_echange">
-    <div class="demande">
+    <div class="demande" v-if="carteDemandee">
       <p>Carte demandée</p>
-      <carte_membre :data="membres[0].card" largeur="4.9vw" />
+      <Carte_membre
+        v-if="carteDemandee._class == 'member'"
+        :data="carteDemandee"
+        largeur="4.9vw"
+      />
+
+      <Carte_familier
+        v-else-if="carteDemandee._class == 'pet'"
+        :data="carteDemandee"
+        largeur="4.9vw"
+      />
+
+      <Carte_terrain
+        v-else-if="carteDemandee._class == 'arena'"
+        :data="carteDemandee"
+        largeur="4.9vw"
+      />
     </div>
     <div class="don">
       <p>Cartes à donner</p>
       <div class="les_cartes">
-        <carte_membre
-          v-for="index in 2"
-          :key="index"
-          class="carte_echange"
-          :data="membres[index].card"
-          largeur="4.9vw"
-        />
+        <div class="carte1" v-if="carteaDonnee1">
+          <Carte_membre
+            v-if="carteaDonnee1._class == 'member'"
+            :data="carteaDonnee1"
+            largeur="4.9vw"
+            class="carte_echange"
+          />
+
+          <Carte_familier
+            v-else-if="carteaDonnee1._class == 'pet'"
+            :data="carteaDonnee1"
+            largeur="4.9vw"
+            class="carte_echange"
+          />
+
+          <Carte_terrain
+            v-else-if="carteaDonnee1._class == 'arena'"
+            :data="carteaDonnee1"
+            largeur="4.9vw"
+            class="carte_echange"
+          />
+        </div>
+        <div class="carte2" v-if="carteaDonnee2">
+          <Carte_membre
+            v-if="carteaDonnee2._class == 'member'"
+            :data="carteaDonnee2"
+            largeur="4.9vw"
+            class="carte_echange"
+          />
+
+          <Carte_familier
+            v-else-if="carteaDonnee2._class == 'pet'"
+            :data="carteaDonnee2"
+            largeur="4.9vw"
+            class="carte_echange"
+          />
+
+          <Carte_terrain
+            v-else-if="carteaDonnee2._class == 'arena'"
+            :data="carteaDonnee2"
+            largeur="4.9vw"
+            class="carte_echange"
+          />
+        </div>
+        <div class="carte3" v-if="carteaDonnee3">
+          <Carte_membre
+            v-if="carteaDonnee3._class == 'member'"
+            :data="carteaDonnee3"
+            largeur="4.9vw"
+            class="carte_echange"
+          />
+
+          <Carte_familier
+            v-else-if="carteaDonnee3._class == 'pet'"
+            :data="carteaDonnee3"
+            largeur="4.9vw"
+            class="carte_echange"
+          />
+
+          <Carte_terrain
+            v-else-if="carteaDonnee3._class == 'arena'"
+            :data="carteaDonnee3"
+            largeur="4.9vw"
+            class="carte_echange"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
-import carte_membre from '@/Composants/carte_membre.vue';
+import { ref } from 'vue';
+import Carte_membre from '@/Composants/carte_membre.vue';
+import Carte_familier from '@/Composants/carte_familier.vue';
+import Carte_terrain from '@/Composants/carte_terrain.vue';
+import config from '@/config.json';
 
-defineProps({
-  carteDemandee: Object,
-  cartesADonner: Object,
+const props = defineProps({
+  echange_id: Number,
 });
 
-const userData = ref(JSON.parse(sessionStorage.getItem('userData')));
+let monEchange = ref(null);
+const touteCartes = ref(null);
+obtenirMesEchanges();
+obtenirCartes();
 
-const collection = computed(() => {
-  return userData.value.collection;
-});
+let carteDemandee = ref(null);
+let carteaDonnee1 = ref(null);
+let carteaDonnee2 = ref(null);
+let carteaDonnee3 = ref(null);
 
-const membres = computed(() => {
-  return collection.value.filter((carte) => carte.card._class === 'member');
-});
+async function obtenirMesEchanges() {
+  try {
+    const response = await fetch(
+      `http://${config.hosts.api}/trade/requests/me`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(),
+      },
+    );
+    const data = await response.json();
+    if (response.ok) {
+      monEchange.value = data.trades.find(
+        (item) => item.tradeRequestId === props.echange_id,
+      );
+    } else {
+      console.error(data);
+    }
+  } catch (erreur) {
+    console.error(erreur);
+  }
+}
+
+async function obtenirCartes() {
+  try {
+    const response = await fetch(`http://${config.hosts.api}/trade/cards`, {
+      method: 'GET',
+      credentials: 'include',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(),
+    });
+    const data = await response.json();
+    if (response.ok) {
+      touteCartes.value = data.cartes;
+      carteDemandee.value = touteCartes.value.find(
+        (item) => item.cardId === monEchange.value.askedCardId,
+      );
+      carteaDonnee1.value = touteCartes.value.find(
+        (item) => item.cardId === monEchange.value.offeredCard1Id,
+      );
+      carteaDonnee2.value = touteCartes.value.find(
+        (item) => item.cardId === monEchange.value.offeredCard2Id,
+      );
+      carteaDonnee3.value = touteCartes.value.find(
+        (item) => item.cardId === monEchange.value.offeredCard3Id,
+      );
+    } else {
+      console.log('erreur');
+    }
+  } catch (erreur) {
+    console.error(erreur);
+  }
+}
 </script>
 
 <style scoped>
