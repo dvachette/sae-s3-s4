ff6566ef827e3158c29d123e5c7ed81196ef4f88
Donatien Vachette <donatien.vachette@hotmail.com>
2026-01-18 11:22:35 +0100

    Fix ouverture booster

--- TCG_web/src/ouverture.vue
+++ TCG_web/src/ouverture.vue
@@ -153,20 +153,33 @@ async function fetchBooster() {
   nbCles.value += data.keys;
   obtainedKeys.value = data.keys;
   sessionStorage.setItem('userData', JSON.stringify(userData.value));
-  for (const cardInfo of data.cards) {
+    for (const cardInfo of data.cards) {
     if (cardInfo._class == 'member') {
       console.log('member');
       const card = createCardMember(cardInfo.cardId, cardInfo);
+      if (cards.find(c => c.id === card.id)) {
+        console.log('Carte déjà présente, changer l\'ID');
+        card.id = `${card.id}-${Date.now()}`; // Ajouter un timestamp pour rendre l'ID unique
+      }
       cards.push(card);
     } else if (cardInfo._class == 'pet') {
       console.log('pet');
       const card = createCardPet(cardInfo.cardId, cardInfo);
+      if (cards.find(c => c.id === card.id)) {
+        console.log('Carte déjà présente, changer l\'ID');
+        card.id = `${card.id}-${Date.now()}`; // Ajouter un timestamp pour rendre l'ID unique
+      }
       cards.push(card);
     } else if (cardInfo._class == 'arena') {
       console.log('arena');
       const card = createCardArena(cardInfo.cardId, cardInfo);
+      if (cards.find(c => c.id === card.id)) {
+        console.log('Carte déjà présente, changer l\'ID');
+        card.id = `${card.id}-${Date.now()}`; // Ajouter un timestamp pour rendre l'ID unique
+      }
       cards.push(card);
     }
+    console.log("Info carte : " , cardInfo);
   }
   console.log('Cartes du booster:', cards);
 }
